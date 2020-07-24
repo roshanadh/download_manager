8ad0ab0ec2ae059f8fbe0f0467ba960c4c5fa179
Roshan <nahsorad@gmail.com>
2020-07-24 23:44:48 +0545

    Create basic CLI for URL input

---
+++ __main__.py
@@ -0,0 +1,28 @@
+from downloader import Downloader
+
+
+def begin_download(download_link):
+	d = Downloader(download_link)
+	# validate url
+	d.download()
+
+
+def main():
+	# download_link = "https://pngimg.com/uploads/google/google_PNG19640.png"
+	# download_link = "https://cdn.pixabay.com/photo/2020/06/02/12/12/sample-5250731_1280.png"
+
+	while True:
+		download_link = input("Type in the download link: \n")
+		begin_prompt = input(f"Begin downloading {download_link} ? y/n ")
+
+		if begin_prompt.lower() == "y":
+			begin_download(download_link)
+
+		elif begin_prompt.lower() == "n":
+			continue
+		else:
+			print("Invalid input\n")
+			continue
+
+if __name__ == "__main__":
+	main()
